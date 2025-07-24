20a245ab99cc35029f5f5e272e2de791cac1fbe1
Sadia Eva <evasadia375@gmail.com>
2025-07-24 19:45:36 -0300

    Committing the game

---
+++ flappe birbe/Assets/pipeSpawnerScript.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pipeSpawnerScript : MonoBehaviour
+{
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate) 
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+
+    // Function spawns pipe 
+    void spawnPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, UnityEngine.Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
+    }
+}
